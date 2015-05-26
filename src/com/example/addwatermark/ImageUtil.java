9660d78303d320c5924a7dd28c2c782c73d41ecd
lixiaoqiang <lxq_xsyu@163.com>
2015-05-26 14:55:28 +0800

    修改了文字的各个方向绘制

--- src/com/example/addwatermark/ImageUtil.java
+++ src/com/example/addwatermark/ImageUtil.java
@@ -12,63 +12,229 @@ import android.graphics.Rect;
 
 public class ImageUtil {
 
-	public static Bitmap createWaterMaskImage(
-			Context Context, Bitmap src, Bitmap watermark,
+	/**
+	 * 设置水印图片在左上角
+	 * @param Context
+	 * @param src
+	 * @param watermark
+	 * @param paddingLeft
+	 * @param paddingTop
+	 * @return
+	 */
+	public static Bitmap createWaterMaskLeftTop(
+			Context context, Bitmap src, Bitmap watermark,
+			int paddingLeft, int paddingTop) {
+		return createWaterMaskBitmap(src, watermark, 
+				dp2px(context, paddingLeft), dp2px(context, paddingTop));
+	}
+	
+	private static Bitmap createWaterMaskBitmap(Bitmap src, Bitmap watermark,
 			int paddingLeft, int paddingTop) {
 		if (src == null) {
 			return null;
 		}
+		int width = src.getWidth();
+		int height = src.getHeight();
 		//创建一个bitmap
-		Bitmap newb = Bitmap.createBitmap(
-				src.getWidth(), src.getHeight(), Config.ARGB_8888);
-				// 创建一个新的和SRC长度宽度一样的位图
+		Bitmap newb = Bitmap.createBitmap(width, height, Config.ARGB_8888);// 创建一个新的和SRC长度宽度一样的位图
 		//将该图片作为画布
-		Canvas cv = new Canvas(newb);
+		Canvas canvas = new Canvas(newb);
 		//在画布 0，0坐标上开始绘制原始图片
-		cv.drawBitmap(src, 0, 0, null);
+		canvas.drawBitmap(src, 0, 0, null);
 		//在画布上绘制水印图片
-		cv.drawBitmap(watermark, paddingLeft, paddingTop, null);
+		canvas.drawBitmap(watermark, paddingLeft, paddingTop, null);
 		// 保存
-		cv.save(Canvas.ALL_SAVE_FLAG);
+		canvas.save(Canvas.ALL_SAVE_FLAG);
 		// 存储
-		cv.restore();
+		canvas.restore();
 		return newb;
 	}
 	
 	/**
-	 * 给图片添加文字
+	 * 设置水印图片在右下角
+	 * @param Context
+	 * @param src
+	 * @param watermark
+	 * @param paddingRight
+	 * @param paddingBottom
+	 * @return
+	 */
+	public static Bitmap createWaterMaskRightBottom(
+			Context context, Bitmap src, Bitmap watermark,
+			int paddingRight, int paddingBottom) {
+		return createWaterMaskBitmap(src, watermark, 
+				src.getWidth() - watermark.getWidth() - dp2px(context, paddingRight), 
+				src.getHeight() - watermark.getHeight() - dp2px(context, paddingBottom));
+	}
+	
+	/**
+	 * 设置水印图片到右上角
+	 * @param Context
+	 * @param src
+	 * @param watermark
+	 * @param paddingRight
+	 * @param paddingTop
+	 * @return
+	 */
+	public static Bitmap createWaterMaskRightTop(
+			Context context, Bitmap src, Bitmap watermark,
+			int paddingRight, int paddingTop) {
+		return createWaterMaskBitmap( src, watermark, 
+				src.getWidth() - watermark.getWidth() - dp2px(context, paddingRight), 
+				dp2px(context, paddingTop));
+	}
+	
+	/**
+	 * 设置水印图片到左下角
+	 * @param Context
+	 * @param src
+	 * @param watermark
+	 * @param paddingLeft
+	 * @param paddingBottom
+	 * @return
+	 */
+	public static Bitmap createWaterMaskLeftBottom(
+			Context context, Bitmap src, Bitmap watermark,
+			int paddingLeft, int paddingBottom) {
+		return createWaterMaskBitmap(src, watermark, dp2px(context, paddingLeft), 
+				src.getHeight() - watermark.getHeight() - dp2px(context, paddingBottom));
+	}
+	
+	/**
+	 * 设置水印图片到中间
+	 * @param Context
+	 * @param src
+	 * @param watermark
+	 * @return
+	 */
+	public static Bitmap createWaterMaskCenter(Bitmap src, Bitmap watermark) {
+		return createWaterMaskBitmap(src, watermark, 
+				(src.getWidth() - watermark.getWidth()) / 2,
+				(src.getHeight() - watermark.getHeight()) / 2);
+	}
+	
+	/**
+	 * 给图片添加文字到左上角
 	 * @param context
 	 * @param bitmap
 	 * @param text
 	 * @return
 	 */
-	public static Bitmap drawTextToBitmap(Context context, Bitmap bitmap, String text) {
-		Resources resources = context.getResources();
-		float scale = resources.getDisplayMetrics().density;
-		//bitmap = scaleWithWH(bitmap, 300 * scale, 300 * scale);
+	public static Bitmap drawTextToLeftTop(Context context, Bitmap bitmap, String text,
+			int size, int color, int paddingLeft, int paddingTop) {
+		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint.setColor(color);
+		paint.setTextSize(dp2px(context, size));
+		Rect bounds = new Rect();
+		paint.getTextBounds(text, 0, text.length(), bounds);
+		return drawTextToBitmap(context, bitmap, text, paint, bounds, 
+				dp2px(context, paddingLeft),  
+				dp2px(context, paddingTop) + bounds.height());
+	}
+	
+	/**
+	 * 绘制文字到右下角
+	 * @param context
+	 * @param bitmap
+	 * @param text
+	 * @param size
+	 * @param color
+	 * @param paddingLeft
+	 * @param paddingTop
+	 * @return
+	 */
+	public static Bitmap drawTextToRightBottom(Context context, Bitmap bitmap, String text,
+			int size, int color, int paddingRight, int paddingBottom) {
+		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint.setColor(color);
+		paint.setTextSize(dp2px(context, size));
+		Rect bounds = new Rect();
+		paint.getTextBounds(text, 0, text.length(), bounds);
+		return drawTextToBitmap(context, bitmap, text, paint, bounds, 
+				bitmap.getWidth() - bounds.width() - dp2px(context, paddingRight), 
+				bitmap.getHeight() - dp2px(context, paddingBottom));
+	}
+	
+	/**
+	 * 绘制文字到右上方
+	 * @param context
+	 * @param bitmap
+	 * @param text
+	 * @param size
+	 * @param color
+	 * @param paddingRight
+	 * @param paddingTop
+	 * @return
+	 */
+	public static Bitmap drawTextToRightTop(Context context, Bitmap bitmap, String text,
+			int size, int color, int paddingRight, int paddingTop) {
+		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint.setColor(color);
+		paint.setTextSize(dp2px(context, size));
+		Rect bounds = new Rect();
+		paint.getTextBounds(text, 0, text.length(), bounds);
+		return drawTextToBitmap(context, bitmap, text, paint, bounds, 
+				bitmap.getWidth() - bounds.width() - dp2px(context, paddingRight), 
+				dp2px(context, paddingTop) + bounds.height());
+	}
+	
+	/**
+	 * 绘制文字到左下方
+	 * @param context
+	 * @param bitmap
+	 * @param text
+	 * @param size
+	 * @param color
+	 * @param paddingLeft
+	 * @param paddingBottom
+	 * @return
+	 */
+	public static Bitmap drawTextToLeftBottom(Context context, Bitmap bitmap, String text,
+			int size, int color, int paddingLeft, int paddingBottom) {
+		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint.setColor(color);
+		paint.setTextSize(dp2px(context, size));
+		Rect bounds = new Rect();
+		paint.getTextBounds(text, 0, text.length(), bounds);
+		return drawTextToBitmap(context, bitmap, text, paint, bounds, 
+				dp2px(context, paddingLeft),  
+				bitmap.getHeight() - dp2px(context, paddingBottom));
+	}
+	
+	/**
+	 * 绘制文字到中间
+	 * @param context
+	 * @param bitmap
+	 * @param text
+	 * @param size
+	 * @param color
+	 * @return
+	 */
+	public static Bitmap drawTextToCenter(Context context, Bitmap bitmap, String text,
+			int size, int color) {
+		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint.setColor(color);
+		paint.setTextSize(dp2px(context, size));
+		Rect bounds = new Rect();
+		paint.getTextBounds(text, 0, text.length(), bounds);
+		return drawTextToBitmap(context, bitmap, text, paint, bounds, 
+				(bitmap.getWidth() - bounds.width()) / 2,  
+				(bitmap.getHeight() + bounds.height()) / 2);
+	}
+	//图片上绘制文字
+	private static Bitmap drawTextToBitmap(Context context, Bitmap bitmap, String text,
+			Paint paint, Rect bounds, int paddingLeft, int paddingTop) {
 		android.graphics.Bitmap.Config bitmapConfig = bitmap.getConfig();
-
-		// set default bitmap config if none
+		
+		paint.setDither(true); // 获取跟清晰的图像采样
+		paint.setFilterBitmap(true);// 过滤一些
 		if (bitmapConfig == null) {
 			bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
 		}
-		// resource bitmaps are imutable,
-		// so we need to convert it to mutable one
 		bitmap = bitmap.copy(bitmapConfig, true);
-
 		Canvas canvas = new Canvas(bitmap);
-		// new antialised Paint
-		Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		// text color - #3D3D3D
-		paint.setColor(Color.RED);
-		paint.setTextSize((int) (18 * scale));
-		paint.setDither(true); // 获取跟清晰的图像采样
-		paint.setFilterBitmap(true);// 过滤一些
-		Rect bounds = new Rect();
-		paint.getTextBounds(text, 0, text.length(), bounds);
-		int x = 30;
-		int y = 150;
-		canvas.drawText(text, x * scale, y * scale, paint);
+		
+		canvas.drawText(text, paddingLeft, paddingTop, paint);
 		return bitmap;
 	}
 	
@@ -97,4 +263,15 @@ public class ImageUtil {
             return Bitmap.createBitmap(src, 0, 0, width, height, matrix, true);
         }
     }
+	
+	/**
+	 * dip转pix
+	 * @param context
+	 * @param dp
+	 * @return
+	 */
+	public static int dp2px(Context context, float dp) { 
+	    final float scale = context.getResources().getDisplayMetrics().density; 
+	    return (int) (dp * scale + 0.5f); 
+	} 
 }
--- src/com/example/addwatermark/MainActivity.java
+++ src/com/example/addwatermark/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.addwatermark;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -28,13 +29,20 @@ public class MainActivity extends Activity {
     	Bitmap sourBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sour_pic);
     	mSourImage.setImageBitmap(sourBitmap);
     	
-    
-    	Bitmap watermarkBitmap = 
-    			ImageUtil.createWaterMaskImage(this, sourBitmap, 
-    					BitmapFactory.decodeResource(getResources(), R.drawable.weixin),
-    					10, 10);
+    	Bitmap waterBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.weixin);
     	
-    	Bitmap textBitmap = ImageUtil.drawTextToBitmap(this, watermarkBitmap, "一段测试文字");
+    	Bitmap watermarkBitmap = ImageUtil.createWaterMaskCenter(sourBitmap, waterBitmap);
+    	watermarkBitmap = ImageUtil.createWaterMaskLeftBottom(this, watermarkBitmap, waterBitmap, 0, 0);
+    	watermarkBitmap = ImageUtil.createWaterMaskRightBottom(this, watermarkBitmap, waterBitmap, 0, 0);
+    	watermarkBitmap = ImageUtil.createWaterMaskLeftTop(this, watermarkBitmap, waterBitmap, 0, 0);
+    	watermarkBitmap = ImageUtil.createWaterMaskRightTop(this, watermarkBitmap, waterBitmap, 0, 0);
+    	
+    	Bitmap textBitmap = ImageUtil.drawTextToLeftTop(this, watermarkBitmap, "左上角", 16, Color.RED, 0, 0);
+    	textBitmap = ImageUtil.drawTextToRightBottom(this, textBitmap, "右下角", 16, Color.RED, 0, 0);
+    	textBitmap = ImageUtil.drawTextToRightTop(this, textBitmap, "右上角", 16, Color.RED, 0, 0);
+    	textBitmap = ImageUtil.drawTextToLeftBottom(this, textBitmap, "左下角", 16, Color.RED, 0, 0);
+    	textBitmap = ImageUtil.drawTextToCenter(this, textBitmap, "中间", 16, Color.RED);
+    	
     	mWartermarkImage.setImageBitmap(textBitmap);
     }
 }
